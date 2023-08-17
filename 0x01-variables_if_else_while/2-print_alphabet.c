0e88f22b47f83c6e49fc528bc38a756067694f49
Mohammed Sada <sadabiye@gmail.com>
2023-08-17 14:20:47 -0700

    variables, if,else and while 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * Prints alphabets in lowercase followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char lower_case;
+
+	for(lower_case = 'a'; lower_case <= 'z', lower_case++);
+	putchar(lower_case);
+	putchar("\n")
+
+	return (0);
+
+}
