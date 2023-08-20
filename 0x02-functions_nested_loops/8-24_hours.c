3f4c884145b3a67aae15e53e7de98e970cc8e10f
Mohammed Sada <sadabiye@gmail.com>
2023-08-20 21:40:12 +0000

    functions and nested loops 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * jac_bauer - Prints every minute of the day of Jack Bauer, starting from
+ * jack_bauer - Prints every minute of the day of Jack Bauer, starting from
  * 00:00 to 23:59
  * hr = hour, mn = minutes
  * / 10 allows second digit to rotate
