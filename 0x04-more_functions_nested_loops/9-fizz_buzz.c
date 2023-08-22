c512196e84e82a6d5b88d0543836d70c494165f0
Mohammed Sada <sadabiye@gmail.com>
2023-08-22 17:05:28 +0000

    More functions and nested loops 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio>
+
+/**
+ * main - Program solves fizz buzz challenge.
+ * For multipes of 3 print Fizz, for multiples of 5 print Buzz.
+ * For multiples of both 3 and 5 print FizzBuzz.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 1;
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else
+			printf("%d", i);
+
+		if (i != 100)
+			printf(" ");
+		i++
+	}
+	printf("\n");
+
+	return (0);
+}
