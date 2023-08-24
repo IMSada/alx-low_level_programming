c9ad6b2aa434c10c40f83d0d871e6666e5d2cbf0
Mohammed Sada <sadabiye@gmail.com>
2023-08-24 20:34:49 +0000

    Pointer, arrays and strings 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rev_string - function reverses a string.
+ * @s: array string type character.
+ */
+
+void rev_string(char *s)
+{
+	int i, j, k;
+	char c
+	
+	for (i = 0; s[i] != '\0'; i++);
+
+	k = i;
+	for (i--, j = 0; j < 1 / 2; i--, j++)
+	{
+		c = s[j];
+		s[j] = s[i];
+		s[i] = c;
+	}
+}
