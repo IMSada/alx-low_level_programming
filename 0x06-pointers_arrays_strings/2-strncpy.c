09529c79900811e9c534b2b47c3e3cb11877b9e5
Mohammed Sada <sadabiye@gmail.com>
2023-08-25 11:00:24 +0000

    More Pointer, arrays and strings 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy  a string from index 0 of `dest`.
+ * @dest: string character
+ * @src: string character
+ * @n: number of characters to copy over
+ * Return: `dest` edited string
+ */
+char *strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src [i];
+	
+	for (; n > i; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
