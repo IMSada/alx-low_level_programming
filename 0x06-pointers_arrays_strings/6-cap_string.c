f69ea465fa3971e6e206ceb789e9aa8bd1d6578b
Mohammed Sada <sadabiye@gmail.com>
2023-08-25 14:44:34 +0000

    More Pointer, arrays and strings 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,7 +11,7 @@ char *cap_string(char *str)
 
 	while (str[i])
 	{
-		while (!(str[i] >= 'a' && str[i] <= 'z))
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
 			i++;
 
 		if (str[i - 1] == ' ' ||
@@ -29,6 +29,7 @@ char *cap_string(char *str)
 		    str[i - 1] == '}' ||
 		    i == 0)
 			str[i] -= 32;
+		
 		i++;
 	}
 
