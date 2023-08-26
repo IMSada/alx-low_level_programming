fd38df041a6936e022eb1bcbfac955cf76a402a2
Mohammed Sada <sadabiye@gmail.com>
2023-08-26 20:49:08 +0000

    More Pointer, arrays and strings 103

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -4,8 +4,8 @@ char *add_strings(char *n1, char *n2, char *r, int r_index);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 /**
- * add_strings - adds the numbers stored in two strings.
- * @ni: The string containing the first number to be added.
+ * add_strings - Adds the numbers stored in two strings.
+ * @n1: The string containing the first number to be added.
  * @n2: The string containing the second number to be added.
  * @r: The buffer to store the result.
  * @r_index: The current index of the buffer.
