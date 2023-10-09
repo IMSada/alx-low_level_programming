705d3c3fb2783461268995bad438f28b23b1fbcb
Mohammed Sada <sadabiye@gmail.com>
2023-10-09 09:34:46 +0000

    Bit manipulation 0

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <limits.h>
+
+unsigned int binary_to_uint(const char *b);
+int _putchar(char c);
+
+#endif
